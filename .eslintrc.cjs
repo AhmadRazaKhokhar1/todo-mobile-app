d9f7bcc47d3b7b3695cfd66d7c20eb4515b416b1
Ahmad Raza Khokhar <ahmadrazawebexpert@gmail.com>
2026-03-24 20:06:26 +0000

    TEC-25: Update the repo based on your architecture

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  ignorePatterns: ["node_modules/", ".expo/", "babel.config.js"],
+  rules: {
+    "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+  },
+};
